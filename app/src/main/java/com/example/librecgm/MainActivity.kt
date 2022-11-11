e7ba12f7e6103c2daf0639712c23bc7c41dd6f74
Seokan <67941435+seokan-jeong@users.noreply.github.com>
2022-11-12 06:10:04 +0900

    tag nfc

--- app/src/main/java/com/example/librecgm/MainActivity.kt
+++ app/src/main/java/com/example/librecgm/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.example.librecgm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "seokan MainActivity"
+    private lateinit var nfcPendingIntent: PendingIntent
+    private lateinit var nfcAdapter: NfcAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        nfcPendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, nfcPendingIntent, null, null);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d(TAG, "on new intent")
+        if (intent.action == NfcAdapter.ACTION_NDEF_DISCOVERED){
+            Log.d(TAG, "discovered")
+            val messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            Log.d(TAG, messages.toString())
+            if (messages == null){
+                return
+            }
+
+            for (i in messages.indices) showMsg(messages[i] as NdefMessage)
+        }
+    }
+
+    fun showMsg(mMessage: NdefMessage) {
+        val recs = mMessage.records
+        for (i in recs.indices) {
+            val record = recs[i]
+            Log.d(TAG, record.toString())
+        }
     }
 }
